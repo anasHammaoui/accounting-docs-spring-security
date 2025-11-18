82dd81ebc6deff921142d318e83e436be6b2657a
anashm <anashammaoui07@gmail.com>
2025-11-18 16:33:11 +0100

    first commit

---
+++ src/main/java/com/example/tricol/accountingdocsspringsecurity/AccountingDocsSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tricol.accountingdocsspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingDocsSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingDocsSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tricol/accountingdocsspringsecurity/AccountingDocsSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tricol.accountingdocsspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingDocsSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
